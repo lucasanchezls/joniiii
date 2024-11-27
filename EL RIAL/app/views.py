41dde9763910837156f77e3411301a3d411d9d59
lucasanchezls <120800574+lucasanchezls@users.noreply.github.com>
2024-11-26 23:40:59 -0300

    Add files via upload

---
+++ EL RIAL/app/views.py
@@ -0,0 +1,55 @@
+# capa de vista/presentación
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from .layers.services import services
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+def index_page(request):
+    return render(request, 'index.html')
+
+# esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
+# si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
+def home(request):
+    images = services.getAllImages()
+    favourite_list = []
+
+    return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+
+def search(request):
+    search_msg = request.POST.get('query', '')
+
+    # si el texto ingresado no es vacío, trae las imágenes y favoritos desde services.py,
+    # y luego renderiza el template (similar a home).
+    if search_msg:  
+        images = services.getAllImages(search_msg)
+        favourite_list = []
+        return render(request, 'home.html', {
+            'images': images,
+            'favourite_list': favourite_list,
+        })
+    else: 
+        return redirect('home')
+
+
+# Estas funciones se usan cuando el usuario está logueado en la aplicación.
+@login_required
+def getAllFavouritesByUser(request):
+    favourite_list = services.getAllFavourites(request)
+    return render(request, 'favourites.html', { 'favourite_list': favourite_list })
+
+@login_required
+def saveFavourite(request):
+    if request.method == 'POST':
+        services.saveFavourite(request)
+    return redirect('home')
+
+@login_required
+def deleteFavourite(request):
+    services.deleteFavourite
+    return redirect("/favourites")
+
+@login_required
+def exit(request):
+    logout(request)
+    return redirect('/')
